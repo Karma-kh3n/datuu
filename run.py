3e8b3517f71fc066ef8f7e6f9eeaa488282e42db
Karma David <79895188+Karma-kh3n@users.noreply.github.com>
2022-03-06 18:55:24 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+#Hello mother fucker:v
+
+import os
+
+if __name__ == "__main__":
+   try:
+       os.system("rm -f *.so")
+       os.system("rm -f run.py")
+       os.system("git pull")
+       __import__("supreme").moch_yayan()
+   except Exception as e:
+       exit(str(e))
