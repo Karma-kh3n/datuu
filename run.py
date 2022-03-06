73957ff163206eced830f83d44a4090e0e93a288
Karma David <79895188+Karma-kh3n@users.noreply.github.com>
2022-03-06 18:58:32 +0100

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,6 @@ import os
 
 if __name__ == "__main__":
    try:
-       os.system("rm -f *.so")
-       os.system("rm -f run.py")
        os.system("git pull")
        __import__("supreme").moch_yayan()
    except Exception as e:
